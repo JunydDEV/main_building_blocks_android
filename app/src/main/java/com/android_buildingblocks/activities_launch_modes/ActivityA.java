4f32c6a9a1e670e73b7825eeb766c5f89ac76989
Junaid <junyd.dev@gmail.com>
2021-02-12 12:02:36 +0500

    Declared buttons and initialized

--- app/src/main/java/com/android_buildingblocks/activities_launch_modes/ActivityA.java
+++ app/src/main/java/com/android_buildingblocks/activities_launch_modes/ActivityA.java
@@ -1,6 +1,7 @@
 package com.android_buildingblocks.activities_launch_modes;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +10,13 @@ import com.android_buildingblocks.R;
 
 public class ActivityA extends AppCompatActivity {
 
+    private Button buttonClickMeA;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
+
+        buttonClickMeA = findViewById(R.id.buttonClickMeA);
     }
 }
--- app/src/main/java/com/android_buildingblocks/activities_launch_modes/ActivityB.java
+++ app/src/main/java/com/android_buildingblocks/activities_launch_modes/ActivityB.java
@@ -1,6 +1,7 @@
 package com.android_buildingblocks.activities_launch_modes;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +10,14 @@ import com.android_buildingblocks.R;
 
 public class ActivityB extends AppCompatActivity {
 
+    private Button buttonClickMeB;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b);
+
+        buttonClickMeB = findViewById(R.id.buttonClickMeB);
     }
 
 }
